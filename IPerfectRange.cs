48ae2597c27c09f74bd66dce9784a2ccdfdd0396
Michael Nolan <48389805+ssjarchon@users.noreply.github.com>
2026-04-18 20:37:32 -0400

    Create IPerfectRange interface for number types

--- IPerfectRange.cs
+++ IPerfectRange.cs
@@ -3,6 +3,7 @@ using System.Numerics;
 namespace PerfectProof;
 
 public interface IPerfectRange<TStart, TEnd, TOffset>
+    : IPerfectRange
     where TStart : INumber<TStart>
     where TEnd : INumber<TEnd>
     where TOffset : INumber<TOffset>
